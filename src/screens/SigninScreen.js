06791c0b0297ba9e83c22f07ca7449cb2bb519fd
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2021-11-08 17:18:49 -0400

    first commit

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SignScreen = () => {
+  return (
+    <View>
+      <Text>Signin Screen</Text>
+    </View>
+  );
+};
+
+export default SignScreen;
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SignScreen = () => {
+  return (
+    <View>
+      <Text>Signup Screen</Text>
+    </View>
+  );
+};
+
+export default SignScreen;
